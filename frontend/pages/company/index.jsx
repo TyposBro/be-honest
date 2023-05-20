3d4dd6d4ea9fff9bb0b4e4699af200bfea261493
Azizbek <khasanjonovich@gmail.com>
2023-05-20 21:09:47 +0900

    foo

--- frontend/pages/company/index.jsx
+++ frontend/pages/company/index.jsx
@@ -1,5 +1,38 @@
+import "regenerator-runtime/runtime";
+import { wallet, contractId } from "../../wallet";
+import { useEffect, useState } from "react";
+
 const Company = () => {
-  return <div>Company</div>;
+  const [id, setid] = useState("");
+
+  // use the wallet to send the greeting to the contract
+  const hire = () => {
+    wallet
+      .callMethod({ method: "hireEmployee", args: { employee_id: id }, contractId })
+      .then((d) => console.log(d))
+      .finally(() => {
+        alert("Hired!");
+      });
+  };
+
+  const get = () => {
+    wallet.callMethod({ method: "getEmployees", contractId }).then((d) => console.log(d));
+  };
+
+  useEffect(() => {
+    wallet
+      .startUp()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <input type="text" onChange={setid} />
+      <button onClick={hire}>Hire!</button>
+      <button onClick={get}>Get!</button>
+    </>
+  );
 };
 
 export default Company;
